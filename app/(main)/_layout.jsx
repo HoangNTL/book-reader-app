fd94407c28c033f9a71ade66d8674f06664d1eda
Hoangcutedz <hoangn2422@gmail.com>
2025-04-07 12:57:53 +0700

    Config font, eslint, tab

---
+++ app/(main)/_layout.jsx
@@ -0,0 +1,28 @@
+import { Stack } from 'expo-router'
+import { Inter_900Black, useFonts } from '@expo-google-fonts/inter'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+
+SplashScreen.preventAutoHideAsync()
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Inter_900Black
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  )
+}
